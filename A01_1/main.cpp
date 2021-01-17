58d7138a1a2c3c2019262782349d447f51618471
Hung Phan <hung.phan@tuni.fi>
2021-01-17 22:10:12 +0700

    Initial Commit

---
+++ A01_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  A01_1
+//
+//  Created by WIN on 17/01/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
